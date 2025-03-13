c1149467fff99be02da2745db46c2a4530c5ac91
jgcruz81 <juangcruz81@gmail.com>
2025-03-12 21:29:53 -0400

    Remove node_modules from git

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Website</h1>
+        <p>This is now a Next.js app with backend capabilities!</p>
+      </div>
+    );
+  }
+  
